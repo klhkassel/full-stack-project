f2c58628870b2b5ce7cca0afe6ab464f339692d5
Elouisa Cardano <iam3jc@gmail.com>
2022-09-23 23:36:45 -0400

    installed request - simplified http client for API call and succesfully renderd first api data base to server

--- app.js
+++ app.js
@@ -10,13 +10,28 @@ const server = http.createServer(app)
 const pgp = require("pg-promise")();
 
 const es6Renderer = require('express-es6-template-engine');
+const { read } = require("fs");
 app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'html');
+app.use(express.json())
 
 
-app.get("/", (req, res) => {
-    res.send("home")
+ 
+app.get("/result",  (request, res) => {
+var request = require('request');
+var muscle = 'biceps';
+request.get({
+  url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
+  headers: {
+    'X-Api-Key': 'GNwhKoMr3v0Eo7kYxoFStg==fm74587zcC86VBO0'
+  },
+}, function(error, response, body) {
+  if(error) return console.error('Request failed:', error);
+  else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+  else res.json(body)
+})
+
 })
 
 
