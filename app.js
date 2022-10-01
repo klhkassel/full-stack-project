3fdbdd47053e9aa1815b456e5b385689ce08747c
ejcGH <iam3jc@gmail.com>
2022-10-01 14:20:05 -0400

    app.post for days of week button

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const http = require("http");
-const hostname = "127.0.0.1";
-const port = 3000;
+const hostname = "127.0.0.1"
+const port = 3000
 
 // const db = pgp("postgres://localhost:5432/workout");
-const express = require("express");
-
+const express = require("express")
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app)
 
+const pgp = require("pg-promise")();
 
-const es6Renderer = require("express-es6-template-engine");
+const es6Renderer = require('express-es6-template-engine');
 const { read } = require("fs");
-app.engine("html", es6Renderer);
-app.set("views", "templates");
-app.set("view engine", "ejs");
-app.use(express.json());
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'ejs');
+app.use(express.json())
 
 
+
+ 
 // app.get("/result",  (request, res) => {
 // var request = require('request');
 // var muscle = 'biceps';
@@ -33,53 +35,67 @@ app.use(express.json());
 
 // })
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 let exerciseData = require("./data/workoutsdb");
-const { Connection } = require("pg");
 
 app.get("/workout_builder", (req, res) => {
-  res.render("workout-builder", { exerciseData });
+  res.render("workout-builder", {exerciseData})
 });
+
+app.post("/workout_buider", (req, res) => {
+  
+
+})
+
 
 app.get("/", (req, res) => {
-  res.render("index", { exerciseData });
-});
+  res.render("index")
+})
 
 app.get("/about_us", (req, res) => {
+  res.render("about-us")
+})
 
-  res.render("about-us");
-});
+app.get("/blog_detail", (req, res) => {
+  res.render("blog-detail")
+})
 
+app.get("/blog_list", (req, res) => {
+  res.render("blog-list")
+})
 
 app.get("/bmi_calc", (req, res) => {
-  res.render("bmi-calculator");
-});
+  res.render("bmi-calculator")
+})
 
 app.get("/contact", (req, res) => {
-  res.render("contact");
-});
+  res.render("contact")
+})
 
 app.get("/faq", (req, res) => {
-  res.render("faq");
-});
+  res.render("faq")
+})
 
 app.get("/exercises/:id", (req, res) => {
-  let id = req.params.id;
-  let workouts = [];
-  exerciseData.categories.forEach((category) => {
-    if (category.type === id) {
-      workouts = category.exercises;
+  let id = req.params.id
+  let workouts = []
+  exerciseData.categories.forEach(category => {
+    if(category.type === id) {
+      workouts = category.exercises
     }
-  });
-  res.render("exercises", { id, workouts });
-});
+  })
+  res.render("exercises", {id, workouts})
+})
+
+
+
 
 // For invalid routes
-app.get("*", (req, res) => {
-  res.send("404! This is an invalid URL.");
-});
+app.get('*', (req, res) => {
+    res.send('404! This is an invalid URL.');
+  });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
