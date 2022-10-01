492196aacc8be3324dc8f24ce390a35ff7bbad12
Your Name <you@example.com>
2022-10-01 12:04:00 -0400

    schema2

--- schema2.sql
+++ schema2.sql
@@ -5,3 +5,4 @@ CREATE TABLE Workout Input (
         muscle text,
         difficulty text,
     );
+
--- seed.sql
+++ seed.sql
@@ -9,52 +9,52 @@ values
     ('Jawann', 'Jawann@gmail.com')
 ;
 
-insert into posts
-    (url, user_id)
-values
-    ('cardio.jpg', 2),
-    ('strength.jpg', 3),
-    ('stretching.jpg', 1),
-    ('weightlifting.jpg', 4),
-    ('plyometrics.jpg', 5),
-    ('powerlifting.jpg', 6)
-;
+-- insert into posts
+--     (url, user_id)
+-- values
+--     ('cardio.jpg', 2),
+--     ('strength.jpg', 3),
+--     ('stretching.jpg', 1),
+--     ('weightlifting.jpg', 4),
+--     ('plyometrics.jpg', 5),
+--     ('powerlifting.jpg', 6)
+-- ;
 
-insert into tags
-    (tag)
-values
-    ('cardio'),
-    ('powerlifting'),
-    ('plyometrics'),
-    ('strength'),
-    ('weightlifting'),
-    ('stretching');
+-- insert into tags
+--     (tag)
+-- values
+--     ('cardio'),
+--     ('powerlifting'),
+--     ('plyometrics'),
+--     ('strength'),
+--     ('weightlifting'),
+--     ('stretching');
 
-insert into tags_posts 
-    (tag_id, post_id)
-values
-    (1, 2),
-    (2, 2),
-    (1, 4),
-    (3, 3),
-    (3, 5);
+-- insert into tags_posts 
+--     (tag_id, post_id)
+-- values
+--     (1, 2),
+--     (2, 2),
+--     (1, 4),
+--     (3, 3),
+--     (3, 5);
 
-insert into likes
-    (user_id, post_id)
-values
-    (1, 1),
-    (1, 2),
-    (2, 3),
-    (4, 5),
-    (2, 2),
-    (3, 1);
+-- insert into likes
+--     (user_id, post_id)
+-- values
+--     (1, 1),
+--     (1, 2),
+--     (2, 3),
+--     (4, 5),
+--     (2, 2),
+--     (3, 1);
 
-insert into comments
-    (comment, post_id, user_id)
-values
-    ('awesome!', 5, 2),
-    ('Great workout!!', 2, 1),
-    ('Nice!', 3, 4),
-    ('Keep going!', 4, 2),
-    ('what is your workout plan?', 4, 1),
-    ('Lets get it!', 3, 5);
+-- insert into comments
+--     (comment, post_id, user_id)
+-- values
+--     ('awesome!', 5, 2),
+--     ('Great workout!!', 2, 1),
+--     ('Nice!', 3, 4),
+--     ('Keep going!', 4, 2),
+--     ('what is your workout plan?', 4, 1),
+--     ('Lets get it!', 3, 5);
