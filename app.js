8a3b43d8fb9ff726dd751f502c38eb477269cb8d
ejcGH <iam3jc@gmail.com>
2022-10-06 18:53:04 -0400

    testing authentication

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 const http = require("http");
 const hostname = "127.0.0.1"
 const port = 3000
-
-// const db = pgp("postgres://localhost:5432/workout");
+const pgp = require("pg-promise")();
+const db = pgp("postgres://localhost:5432/workout");
 const express = require("express")
 const app = express();
 const server = http.createServer(app)
 
-const pgp = require("pg-promise")();
-
 const es6Renderer = require('express-es6-template-engine');
 const { read } = require("fs");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const cookieParser = require("cookie-parser")
+
+
 app.engine('html', es6Renderer);
 app.set('views', 'templates');
 app.set('view engine', 'ejs');
 app.use(express.json())
 
+app.use(cookieParser());
+app.use(session({
+  secret: "secret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    maxAge: 259200
+  }
+}))
+
+let hashedPassword = ""; 
+bcrypt.hash("password", 10, (err, hash) => {
+  hashedPassword = hash
+  console.log(hash)
+})
 
 
- 
-// app.get("/result",  (request, res) => {
-// var request = require('request');
-// var muscle = 'biceps';
-// request.get({
-//   url: 'https://api.api-ninjas.com/v1/exercises?muscle=' + muscle,
-//   headers: {
-//     'X-Api-Key': 'GNwhKoMr3v0Eo7kYxoFStg==fm74587zcC86VBO0'
-//   },
-// }, function(error, response, body) {
-//   if(error) return console.error('Request failed:', error);
-//   else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
-//   else res.json(body)
-// })
-
-// })
 
 app.use(express.static('public'));
 
@@ -48,9 +51,14 @@ app.post("/workout_buider", (req, res) => {
 
 })
 
-
-app.get("/", (req, res) => {
-  res.render("index", {exerciseData})
+// TRY AND GET THIS TO WORK 
+app.get("/", checkAuth, (req, res) => {
+  if(req.session.username) {
+    res.render("index", {exerciseData},)
+  } else {
+    res.send("incorrect")
+  }
+  
 })
 
 app.get("/about_us", (req, res) => {
@@ -88,8 +96,41 @@ app.get("/exercises/:id", (req, res) => {
   res.render("exercises", {id, workouts})
 })
 
+// app.post("/exercises/:id", {})
 
+app.get("/login", checkAuth, (req, res) => {
+  res.render("login")
+})
 
+app.post("/login", (req, res) => {
+  db.any("SELECT * from users").then((users) => console.log(users));
+  const { username, password } = req.body;
+
+  if (username == "user") {
+    bcrypt.compare(password, hashedPassword, (err, match) => {
+      if (match) {
+            req.session.username = username;
+            res.redirect("/workout_buider")
+      } else {
+        res.render("/login")
+      }
+    })
+  } else {
+    res.send("unknown user")
+  }
+
+})
+
+function checkAuth(req, res, next) {
+  if(req.session.username) {
+    next();
+  } else if(req.path == "/login") {
+    next();
+  } else {
+    res.redirect("/login")
+  }
+
+}
 
 // For invalid routes
 app.get('*', (req, res) => {
---
+++ builder.js
@@ -0,0 +1,7 @@
+const addToCalandarBtn = document.getElementById("addToCalandar")
+
+addToCalandarBtn.addEventListener("click", clickTest)
+
+function clickTest() {
+    console.log("this was clicked")
+}
