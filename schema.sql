cacb6d4762cee802be08dd172fc2581980ee5190
Your Name <you@example.com>
2022-10-08 10:10:06 -0400

    schema

--- schema.sql
+++ schema.sql
@@ -1,10 +1,9 @@
-create table users (
+create table newsletters. users (
     id serial primary key,
     name varchar(50) not null,
     email varchar(50) not null unique,
     password_digest text not null
 );
-
 
 
 -- CREATE TABLE newsletter . users (
