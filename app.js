7fed4e508e6386db09a6acd6334fb909402b7d27
klhkassel <klhkassel@gmail.com>
2022-10-10 07:47:16 -0700

    working on authentication

--- app.js
+++ app.js
@@ -1,78 +1,136 @@
 const http = require("http");
-const hostname = "127.0.0.1"
-const port = 3000
+const hostname = "127.0.0.1";
+const port = 3000;
 
-const express = require("express")
+const express = require("express");
 const app = express();
-const server = http.createServer(app)
+const server = http.createServer(app);
 
 const pgp = require("pg-promise")();
 const db = pgp("postgres://localhost:5432/workout");
 
-const es6Renderer = require('express-es6-template-engine');
-const { read } = require("fs");
-app.engine('html', es6Renderer);
-app.set('views', 'templates');
-app.set('view engine', 'ejs');
-app.use(express.json())
+//login authentication
+const database = require("../models");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 
-app.use(express.static('public'));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUnititalized: true,
+    cookie: {
+      secure: false,
+      maxAge: 2592000,
+    },
+  })
+);
+
+bcrypt.hash(myPassword, saltRounds, (err, hash) => {});
+
+//
+
+const es6Renderer = require("express-es6-template-engine");
+const { read } = require("fs");
+app.engine("html", es6Renderer);
+app.set("views", "templates");
+app.set("view engine", "ejs");
+app.use(express.json());
+
+app.use(express.static("public"));
 
 let exerciseData = require("./data/workoutsdb");
 
 app.get("/workout_builder", (req, res) => {
-  res.render("workout-builder", {exerciseData})
+  res.render("workout-builder", { exerciseData });
 });
 
-app.post("/workout_buider", (req, res) => {
-  
-
-})
-
+app.post("/workout_buider", (req, res) => {});
 
 app.get("/", (req, res) => {
-  res.render("index", {exerciseData})
-})
+  res.render("index", { exerciseData });
+});
 
 app.get("/about_us", (req, res) => {
-  res.render("about-us")
-})
+  res.render("about-us");
+});
 
 app.get("/blog_detail", (req, res) => {
-  res.render("blog-detail")
-})
+  res.render("blog-detail");
+});
 
 app.get("/blog_list", (req, res) => {
-  res.render("blog-list")
-})
+  res.render("blog-list");
+});
 
 app.get("/bmi_calc", (req, res) => {
-  res.render("bmi-calculator")
-})
+  res.render("bmi-calculator");
+});
 
 app.get("/contact", (req, res) => {
-  res.render("contact")
-})
+  res.render("contact");
+});
+
+//login
+app.post("/contact", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  findUser(email, password)
+    .then((user) => {
+      req.session.email = user;
+      res.redirect("/index");
+    })
+    .catch((err) => {
+      res.send("login failure");
+    });
+});
+
+//login page: storing and comparing email and password,and redirecting to home page after login
+app.post("/user", function (req, res) {
+  db.User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then(function (user) {
+    if (!user) {
+      res.redirect("/contact");
+    } else {
+      bcrypt.compare(req.body.password, user.password, function (err, result) {
+        if (result == true) {
+          res.redirect("/index");
+        } else {
+          res.send("Incorrect password");
+          res.redirect("/");
+        }
+      });
+    }
+  });
+});
+//
 
 app.get("/faq", (req, res) => {
-  res.render("faq")
-})
+  res.render("faq");
+});
 
 app.get("/exercises/:id", async (req, res) => {
-  let id = req.params.id
-  let workouts = []
-  let benefits = []
+  let id = req.params.id;
+  let workouts = [];
+  let benefits = [];
 
   let dayofweek = await db.any("SELECT * from dayofweek");
   console.log(dayofweek);
-  exerciseData.categories.forEach(category => {
-    if(category.type === id) {
-      workouts = category.exercises
-      benefits = category.benefits
+  exerciseData.categories.forEach((category) => {
+    if (category.type === id) {
+      workouts = category.exercises;
+      benefits = category.benefits;
     }
-  })
-  res.render("exercises", {id, workouts, benefits, dayofweek})
-  })
+  });
+  res.render("exercises", { id, workouts, benefits, dayofweek });
+});
 
 app.post("/exercises", async (req, res) => {
   const params = await req.body.json();
@@ -80,8 +138,10 @@ app.post("/exercises", async (req, res) => {
   const exerciseId = params.exerciseId;
   const selectedDays = params.daysOfWeek;
 
-  selectedDays.forEach(day => {
-    db.query(`INSERT INTO daysOfWeek_Exercises(workout_input, dayofweek) VALUES(${exerciseId}, ${day})`);
+  selectedDays.forEach((day) => {
+    db.query(
+      `INSERT INTO daysOfWeek_Exercises(workout_input, dayofweek) VALUES(${exerciseId}, ${day})`
+    );
   });
 
   res.sendStatus(200);
@@ -92,13 +152,11 @@ app.post("/exercises", async (req, res) => {
 //   res.json(records);
 // });
 
-
-
 // For invalid routes
-app.get('*', (req, res) => {
-    res.send('404! This is an invalid URL.');
-  });
+app.get("*", (req, res) => {
+  res.send("404! This is an invalid URL.");
+});
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
